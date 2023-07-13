974112df04a973fd24d1f3fa13cb3fade52d1b6d
Evrengo <evrengencoglu21@gmail.com>
2023-07-13 19:08:59 +0300

    Diğer Controller atamalarıda yapıldı.

---
+++ NETCoreMVCBlog/Controllers/YorumController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETCoreMVCBlog.Models;
+
+namespace NETCoreMVCBlog.Controllers
+{
+    public class YorumController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            Yorum y1 = new Yorum() { Yazi="Harika!", YorumTarihi=Convert.ToDateTime("13.07.2023"), YorumBegeni=47};
+            return View();
+        }
+    }
+}
